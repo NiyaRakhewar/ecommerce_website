505f8a2c983fac74dc208ffcd51a67c68bb6be83
Niharika Rakhewar <111850896+NiyaRakhewar@users.noreply.github.com>
2023-06-07 02:08:05 +0530

    update

--- src/context/HomeContext.jsx
+++ src/context/HomeContext.jsx
@@ -1,13 +1,6 @@
 import { createContext, useReducer } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { reducer, initialState } from "./../Reducer/reducer";
-=======
-import { reducer, initialState } from "../reducer/reducer";
->>>>>>> 373e5ec2a27b3c6c0f38e7d0ced2abbc0ad1f101
-=======
+
 import { reducer, initialState } from "../Reducer/reducer";
->>>>>>> 36d633544b9a669255e4a64a106ff5ac11df24c6
 
 export const HomeContext = createContext();
 
